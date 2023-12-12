4590bfbc63a6bbe601f7cf12ebb7ede8bcee48ad
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-12-12 18:10:46 +0000

    投稿機能　編集中

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,27 +6,72 @@ class Public::PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    @post.save
-    redirect_to post_path(@post.id)
+
+    tag_list = params[:post][:name].split(',')
+
+    if params[:draft].present?
+      @post.post_status = :draft
+    else
+      @post.post_status = :active
+    end
+
+    if @post.save
+      if @post.draft?
+        @post.save_tag(tag_list)
+        redirect_to post_path(@post.id), notice: '下書きが保存されました。'
+      else
+        @post.save_tag(tag_list)
+        redirect_to post_path(@post.id), notice: '投稿が公開されました。'
+      end
+    else
+      render :new
+    end
   end
 
   def index
-    @posts = Post.all
+    post = Post.where(post_status: 2)
+    @posts = post.all
+    @tags = Tag.all
   end
 
   def show
     @post = Post.find(params[:id])
+    @tags = @post.tags
   end
 
   def edit
     @post = Post.find(params[:id])
+    @tag = @post.tags.pluck(:name).join(',')
   end
 
   def update
     @post = Post.find(params[:id])
+    tag = params[:post][:name].split(',')
     @post.user_id = current_user.id
-    @post.update(post_params)
-    redirect_to post_path(@post.id)
+
+    #下書き機能・属性を追加
+    @post.assign_attributes(post_params)
+
+    if params[:draft].present?
+      @post.post_status = :draft
+      notice_message = "下書きを保存しました。"
+      redirect_path = post_path(@post.id)
+    elsif params[:inactive].present?
+      @post.post_status = :inactive
+      notice_message = "非公開にしました。"
+      redirect_path = post_path(@post.id)
+    else params[:active].present?
+      @post.post_status = :active
+      notice_message = "公開にしました。"
+      redirect_path = post_path(@post.id)
+    end
+
+    if @post.update(post_params)
+       @post.save_tag(tag)
+      redirect_to redirect_path, notice: notice_message
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
 
   enum post_status: { draft: 0, inactive: 1, active: 2 }
 
@@ -14,5 +15,23 @@ class Post < ApplicationRecord
       post_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     post_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def save_tag(sent_tags)
+    # タグの存在を確認->タグを配列として取得
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    # 取得したタグから送られてきたタグを除いたタグ
+    old_tags = current_tags - sent_tags
+    # 送信されてきたタグから現在存在するタグを除いたタグ
+    new_tags = sent_tags - current_tags
+
+    old_tags.each do |old|
+      self.tags.delete　Tag.find_by(name: old)
+    end
+
+    new_tags.each do |new|
+      new_post_tag = Tag.find_or_create_by(name: new)
+      self.tags << new_post_tag
+    end
   end
 end
