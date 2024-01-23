0bc1f2c9cb2135200729bc554efc7f298f279704
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2024-01-23 15:50:00 +0000

    コメント非同期

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,18 +2,18 @@ class Public::CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    if comment.save
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = post.id
+    if @comment.save
     else
       flash[:alert] = "コメントに失敗しました。"
     end
   end
 
   def destroy
-    comment = Comment.find(params[:post_id])
-    post = comment.post
-    comment.destroy
+    @comment = Comment.find(params[:id])
+    post = @comment.post
+    @comment.destroy
   end
 
 private
